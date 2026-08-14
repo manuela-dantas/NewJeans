caf226db1a5ce37d7dc3773264d5b479f76bbc72
manuela <manuelarodrigues2025@gmail.com>
2026-08-13 21:26:22 -0300

    mudanças significativas

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-
     path('', views.inicio, name='inicio'),
-
     path('produtos/', views.produtos, name='produtos'),
-
     path('categorias/', views.categorias, name='categorias'),
-
     path('sobre/', views.sobre, name='sobre'),
-
     path('contato/', views.contato, name='contato'),
-
 ]
